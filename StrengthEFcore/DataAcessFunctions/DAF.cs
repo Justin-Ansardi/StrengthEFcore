46f672f3ebcd939e6b41722376d27e43b7d248ed
Justin Ansardi <ansardi.justin@gmail.com>
2024-10-13 16:08:32 -0400

    Basic data access

--- StrengthEFcore/DataAcessFunctions/DAF.cs
+++ StrengthEFcore/DataAcessFunctions/DAF.cs
@@ -9,8 +9,19 @@ namespace StrengthEFcore.DataAcessFunctions
 {
     public static class DAF
     {
+        public static IQueryable<Workout> GetUserWorkoutData(EntityContext db, int userId)
+            => db.Workout.Select(x => x)
+                         .Include(x => x.ExerciseBouts)
+                         .Where(x => x.UserId == userId);
+
+
         public static IQueryable<User> GetAllUsers(EntityContext db)
             => db.Users.Select(x => x);
 
+        public static IQueryable<Workout> GetWorkout(EntityContext db, int id)
+            => db.Workout.Select(x => x).Include(x => x.ExerciseBouts).Where(x => x.Id == id);
+
+        public static IQueryable<ExerciseBout> GetExerciseBouts(EntityContext db, int workoutId)
+            => db.ExerciseBout.Select(x => x).Where(x => x.WorkoutId == workoutId);
     }
 }
