2d2d64fa0550e0134dd7f899df3c759507c72f64
Justin Ansardi <ansardi.justin@gmail.com>
2024-10-13 17:20:14 -0400

    Created Post new user

---
+++ StrengthEFcore/CreationalFucntions/CF.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrengthEFcore.CreationalFucntions
+{
+    public static class CF
+    {
+        public static async Task CreateNewUser(EntityContext db, User user)
+        {
+            await db.Users.AddAsync(user);
+            await db.SaveChangesAsync();
+        }
+          
+      
+    }
+}
