2da100a5f74d209bbb0e44d9edfe5298c8186f39
Justin Ansardi <ansardi.justin@gmail.com>
2024-10-13 13:56:14 -0400

    Set up some simple dev frameworks

---
+++ StrengthEFcore/DataAcessFunctions/DAF.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrengthEFcore.DataAcessFunctions
+{
+    public static class DAF
+    {
+        public static IQueryable<User> GetAllUsers(EntityContext db)
+            => db.Users.Select(x => x);
+
+    }
+}
